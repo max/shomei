3ac63cf7f720186ec1d3dfbf60f69ab5f36563cb
Maximilian Schoening <mschoening@me.com>
2013-05-17 13:56:11 -0700

    Remove comments

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -1,28 +1,20 @@
 class SignaturesController < ApplicationController
   before_action :set_signature, only: [:show, :edit, :update, :destroy]
 
-  # GET /signatures
-  # GET /signatures.json
   def index
     @signatures = Signature.all
   end
 
-  # GET /signatures/1
-  # GET /signatures/1.json
   def show
   end
 
-  # GET /signatures/new
   def new
     @signature = Signature.new
   end
 
-  # GET /signatures/1/edit
   def edit
   end
 
-  # POST /signatures
-  # POST /signatures.json
   def create
     @signature = Signature.new(signature_params)
 
@@ -37,8 +29,6 @@ class SignaturesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /signatures/1
-  # PATCH/PUT /signatures/1.json
   def update
     respond_to do |format|
       if @signature.update(signature_params)
@@ -51,8 +41,6 @@ class SignaturesController < ApplicationController
     end
   end
 
-  # DELETE /signatures/1
-  # DELETE /signatures/1.json
   def destroy
     @signature.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class SignaturesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_signature
       @signature = Signature.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def signature_params
       params.require(:signature).permit(:name, :signature)
     end
