40ff8d7327627a8ad7d22690d465425c7dd47839
Jonathan Clem <jonathan@jclem.net>
2013-05-17 13:34:21 -0700

    Cast signature as JSON string before saving

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -1,6 +1,8 @@
 class Signature < ActiveRecord::Base
-
   validates :name, presence: true
   validates :signature, presence: true
 
+  def signature=(signature)
+    self[:signature] = signature.to_json
+  end
 end
