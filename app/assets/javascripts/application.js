e1da179093f8ada84910c5a7ff8780cf6ac47ea7
Maximilian Schoening <mschoening@me.com>
2013-05-31 01:10:07 -0700

    Allow clearing of signature by shaking

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,44 @@ $(function(){
     lineTop: 140,
     penColour: '#2D2D2D'
   }
-  $('#new_signature').signaturePad(signatureOptions);
+
+  var mySignaturePad = $('#new_signature');
+
+  mySignaturePad.signaturePad(signatureOptions);
+
+  function clearSignatureOnShake() {
+    if (confirm('Clear Signature?')) {
+      mySignaturePad.signaturePad().clearCanvas();
+    }
+  }
+
+  if (typeof window.DeviceMotionEvent != 'undefined') {
+    // Shake sensitivity (a lower number is more)
+    var sensitivity = 35;
+
+    // Position variables
+    var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
+
+    // Listen to motion events and update the position
+    window.addEventListener('devicemotion', function (e) {
+      x1 = e.accelerationIncludingGravity.x;
+      y1 = e.accelerationIncludingGravity.y;
+      z1 = e.accelerationIncludingGravity.z;
+    }, false);
+
+    // Periodically check the position and fire
+    // if the change is greater than the sensitivity
+    setInterval(function () {
+      var change = Math.abs(x1-x2+y1-y2+z1-z2);
+
+      if (change > sensitivity) {
+        clearSignatureOnShake();
+      }
+
+      // Update new position
+      x2 = x1;
+      y2 = y1;
+      z2 = z1;
+    }, 50);
+  }
 });
